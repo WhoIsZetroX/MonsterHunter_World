9867be834c0b8b3ff647a6a0e54b63e61ea450e9
aleix <aleixaragon7@gmail.com>
2018-03-22 16:02:16 +0100

    YoutubeVideo

--- app/src/main/java/com/example/proyectodani2/MonsterVideosFragment.java
+++ app/src/main/java/com/example/proyectodani2/MonsterVideosFragment.java
@@ -4,109 +4,74 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.ImageButton;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 
 public class MonsterVideosFragment extends Fragment {
 
-    VideoView videoView;
-    MediaController mc;
-    ImageButton playImageButton;
-    public MonsterVideosFragment() {
-            // Required empty public constructor
-    }
+    private static final String API_KEY = "AIzaSyBa0iMF2ecFOuZWbTT9dvy9QhDcFh7zR";
+
+    private static String VIDEO_ID = "f2MOYNTe2oU";
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        // Inflate the layout for this fragmentç
-        View rootview = inflater.inflate(R.layout.fragment_monster_videos, container, false);
-        videoView = rootview.findViewById(R.id.video);
-        playImageButton = rootview.findViewById(R.id.playImageButton);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.you_tube_api, container, false);
 
-        videoView.setVisibility(View.INVISIBLE);
+        YouTubePlayerSupportFragment youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance();
 
-        playImageButton.setOnClickListener(new View.OnClickListener() {
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.youtube_layout, youTubePlayerFragment).commit();
+
+        youTubePlayerFragment.initialize(API_KEY, new OnInitializedListener() {
             @Override
-            public void onClick(View view) {
-                videoView.setVisibility(View.VISIBLE);
-                playImageButton.setVisibility(View.INVISIBLE);
-                mc = new MediaController(getActivity());
-                String path = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.n1;
-                videoView.setVideoURI(Uri.parse(path));
-                videoView.setMediaController(mc);
-                ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
-                mc.setAnchorView(videoView);
-                videoView.start();
-                mc.setAnchorView(videoView);
+            public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+                if (!wasRestored) {
+                    player.setPlayerStyle(YouTubePlayer.PlayerStyle.DEFAULT);
+                    player.loadVideo(VIDEO_ID);
+                    player.play();
 
+                    }
+            }
+
+            @Override
+            public void onInitializationFailure(Provider provider, YouTubeInitializationResult error) {
+                // YouTube error
+                String errorMessage = error.toString();
+                Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_LONG).show();
+                Log.d("errorMessage:", errorMessage);
             }
         });
 
-        return rootview;
+        return rootView;
     }
-
-    //TODO: https://www.youtube.com/watch?v=f2MOYNTe2oU
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
-        if(isVisibleToUser) {
-            System.out.println("BISIBLE");
-
-        } else {
-            System.out.println("NO BISIBLE");
-
-            if (videoView!=null){
-                videoView.pause();
-                videoView.setMediaController(null);
-                ((AppCompatActivity)getActivity()).getSupportActionBar().show();
-
-            }
-        }
-    }
-
-    //    @Override
-//    public void onStop() {
-//        super.onStop();
-//        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
-//    }
-
-    /* Video original brian
-
-
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
-        if(isVisibleToUser) {
-            System.out.println("BISIBLE");
-            mc = new MediaController(getActivity());
-
-            String path = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.n1;
-            videoView.setVideoURI(Uri.parse(path));
-            videoView.setMediaController(mc);
-            ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
-
-            videoView.start();
-
-        } else {
-            System.out.println("NO BISIBLE");
-
-            if (videoView!=null){
-                videoView.pause();
-                videoView.setMediaController(null);
-                ((AppCompatActivity)getActivity()).getSupportActionBar().show();
-
-            }
-        }
-    }
-
-
-     */
 
 }
