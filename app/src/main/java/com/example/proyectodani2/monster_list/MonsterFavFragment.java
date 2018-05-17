821633df86e4c481104c7b63dfbe1e7b7d45e43a
aleix <aleixaragon7@gmail.com>
2018-05-17 19:51:14 +0200

    Search update

--- app/src/main/java/com/example/proyectodani2/monster_list/MonsterFavFragment.java
+++ app/src/main/java/com/example/proyectodani2/monster_list/MonsterFavFragment.java
@@ -1,6 +1,10 @@
 package com.example.proyectodani2.monster_list;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.AppBarLayout;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.Query;
@@ -13,7 +17,11 @@ public class MonsterFavFragment extends MonsterFragment {
 
     @Override
     public Query setQuery() {
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) search.getLayoutParams();
+        layoutParams.height = 1;
+        search.setLayoutParams(layoutParams);
         search.setVisibility(View.INVISIBLE);
         return mReference.child("monsters/favoritos").child(FirebaseAuth.getInstance().getUid());
     }
+
 }
--- app/src/main/java/com/example/proyectodani2/monster_list/MonstersAllFragment.java
+++ app/src/main/java/com/example/proyectodani2/monster_list/MonstersAllFragment.java
@@ -1,11 +1,22 @@
 package com.example.proyectodani2.monster_list;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.AppBarLayout;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
 import com.google.firebase.database.Query;
 
 public class MonstersAllFragment extends MonsterFragment {
 
     @Override
     public Query setQuery() {
+
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) search.getLayoutParams();
+        layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        search.setLayoutParams(layoutParams);
+        search.setVisibility(View.VISIBLE);
         return mReference.child("monsters/all");
     }
 }
