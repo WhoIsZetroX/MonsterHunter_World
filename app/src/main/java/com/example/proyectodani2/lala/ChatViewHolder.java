d47726f5741357a15a8aa57ff2f6e4d3b8dcddb4
ZetroX <aleixaragon7@gmail.com>
2018-05-25 02:26:07 +0200

    chat fin

--- app/src/main/java/com/example/proyectodani2/lala/ChatViewHolder.java
+++ app/src/main/java/com/example/proyectodani2/lala/ChatViewHolder.java
@@ -3,6 +3,7 @@ package com.example.proyectodani2.lala;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -11,11 +12,13 @@ import com.example.proyectodani2.R;
 public class ChatViewHolder extends RecyclerView.ViewHolder {
 
     public TextView tvContent, tvContent2;
+    LinearLayout linearLayout;
 
     public ChatViewHolder(View itemView) {
         super(itemView);
         tvContent = itemView.findViewById(R.id.text_);
         tvContent2 = itemView.findViewById(R.id.text2_);
+        linearLayout = itemView.findViewById(R.id.linearLayout);
     }
 
 }
